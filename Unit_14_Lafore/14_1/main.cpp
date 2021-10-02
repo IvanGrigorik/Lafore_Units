42faa47d17f992c381001719efb5ae64ab55ffdd
Ivan <givan502@gmail.com>
2021-10-02 14:59:02 +0300

    Finished

--- Unit_14_Lafore/14_1/main.cpp
+++ Unit_14_Lafore/14_1/main.cpp
@@ -10,9 +10,16 @@ Type avg (Type *arr, int size) {
 }
 
 int main () {
-    float arr_f[] = {1.2, 2.3, 4.2};
-    std::cout << avg(arr_f, 3) << std::endl;
-
-    int arr_i[] = {1, 2, 3, 4, 5, 10, 12};
+    int arr_i[]{1, 2, 3, 4, 5, 10, 12};
     std::cout << avg(arr_i, 7) << std::endl;
+
+
+    long arr_l[]{111111111, 222222222, 433333333};
+    std::cout << avg(arr_l, 3) << std::endl;
+
+    double arr_d[]{1.3, 2.3, 4.4, 2.23};
+    std::cout << avg(arr_d, 4) << std::endl;
+
+    char arr_c[]{1, 2, 3, 4, 5};
+    std::cout << static_cast<int>(avg(arr_c, 5)) << std::endl;
 }
